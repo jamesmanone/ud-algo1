1ecbb32062921eb54bbb5b668f27ab10d2eed71b
JAMES MARTIN <boosterdx@me.com>
2020-04-28 11:40:07 -0400

    problem 3 passing

--- problem3.py
+++ problem3.py
@@ -1,39 +1,49 @@
 #!/usr/bin/env python3
+from random import randint
 
 
-def quicksort(arr, s, e):
-    if e - s < 1:
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+
+def heapify(arr, n, i):
+    left = (i * 2) + 1
+    right = left + 1
+    if left > n:
         return
-    p = arr[-1]
-    if len(arr) > 5:
-        p = (arr[-1] + arr[-2] + arr[-3] + arr[-4] + arr[-5]) / 5
-    s1, e1 = s, e
+    if right > n:
+        if arr[left] > arr[i]:
+            swap(arr, left, i)
+        return
 
-    while s1 < e1:
-        if arr[s1] > p:
-            arr[s1], arr[e1] = arr[e1], arr[s1]
-            e1 -= 1
-        else:
-            s1 += 1
-
-    quicksort(arr, s, s1)
-    quicksort(arr, e1+1, e)
+    max_child = left if arr[left] > arr[right] else right
+    if arr[max_child] > arr[i]:
+        swap(arr, max_child, i)
+        heapify(arr, n, max_child)
 
 
-def rearange_digits(input):
-    input = quicksort(input[:], 0, len(input)-1)
-    out = [0, 0]
-    i = len(input) - 1
-
+def build_max_heap(arr):
+    n = len(arr) - 1
+    i = n//2
     while i >= 0:
-        if i % 2 == 0:
-            out[0] *= 10
-            out[0] += input[i]
-        else:
-            out[1] *= 10
-            out[1] += input[i]
+        heapify(arr, n, i)
+        i -= 1
 
-    return out
+
+def rearange_digits(arr):
+    build_max_heap(arr)
+    n = len(arr)-1
+
+    out = [0, 0]
+    while n >= 0:
+        largest = arr[0]
+        swap(arr, n, 0)
+        n -= 1
+        heapify(arr, n, 0)
+
+        out[n % 2] *= 10
+        out[n % 2] += largest
+    return out if out[0] > out[1] else [out[1], out[0]]
 
 
 def test(input, expected, tst):
@@ -45,15 +55,17 @@ def test(input, expected, tst):
         print(f'\u274C Expected {expected}, got {actual} for {input}')
         return True
 
-# [3 4 5 6 6 8 9 9]
+
 def main():
     test_list = [
         ([1, 2, 3, 4, 5], [531, 42]),
-        ([6, 5, 9, 6, 8, 3, 4, 9], [9653, 9864]),
-        ([4, 6, 2, 5, 9, 8], [852, 964])
+        ([6, 5, 9, 6, 8, 3, 4, 9], [9864, 9653]),
+        ([4, 6, 2, 5, 9, 8], [964, 852])
     ]
     for q, a in test_list:
         test(q, a, rearange_digits)
+
+    print("passed \U0001F60E")
 
 
 if __name__ == '__main__':
